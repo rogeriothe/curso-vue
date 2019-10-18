e041daade88200c5ca2a894509b50065ec79d0e3
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-10-18 11:31:24 -0300

    aula 94

--- vue-cli/projeto-simples/src/App.vue
+++ vue-cli/projeto-simples/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ titulo }}
+    <button @click="titulo += '!'">Alterar</button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      titulo: 'Hello World!'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    background-color: #067BC2;
+    color: #ECC30B;
+  }
 </style>
