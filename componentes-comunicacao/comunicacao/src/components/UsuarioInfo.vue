f284dd5dff03b204b975a249d9a6c83b1efb7098
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-10-26 10:05:15 -0300

    aula 120

---
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>Nome de Usuário: <strong>{{ inverterNome() }}</strong> </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      nome: {
+          type: String,
+          //required: true,
+          //defaut: 'Anônimo'
+          //   default: function() {
+          //       return 'Mr. Zé'
+          //   }
+      }
+  },
+  methods: {
+      inverterNome() {
+          return this.nome.split('').reverse().join('')
+      }
+  },
+}
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
