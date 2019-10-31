8870ffacf30f94e701270783d5051f318075d5cd
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-10-31 10:44:02 -0300

    comunicação direta via props concluida

--- componentes-comunicacao/comunicacao-direta/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao-direta/src/components/UsuarioInfo.vue
@@ -2,12 +2,37 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome de usuário: <strong>{{ modificaNome() }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    // 1ª Forma
+    //props: ['nome'],
+
+    // 2ª Forma
+    props: {
+        // Pode receber um tipo
+        // nome: String
+        // ou
+        // Pode receber varios tipos
+        // nome: [String, Boolean]
+        nome: {
+            type: String,
+            // required: true,
+            // default: 'Chico'
+            // default pode ser uma funcao
+            default: function() {
+                return 'Mr' + this.nome
+            }
+        }        
+    },
+    methods: {
+        modificaNome() {
+            return this.nome.toUpperCase()
+        }
+    }
 }
 </script>
 
