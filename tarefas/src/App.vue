2a4ba6d2174d0654a9ce5b4013eeea31c857a99f
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-11-09 11:14:04 -0300

    secao 10 concluida

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Tarefas</h1>
+    <TasksProgress :progress="progress" />
     <NewTask @taskAdded="addTask" />
     <TaskGrid
       :tasks="tasks"
@@ -11,14 +12,30 @@
 </template>
 
 <script>
+import TasksProgress from '@/components/TasksProgress.vue'
 import TaskGrid from '@/components/TaskGrid.vue'
 import NewTask from '@/components/NewTask.vue'
 
 export default {
-  components: { TaskGrid, NewTask },
+  components: { TaskGrid, NewTask, TasksProgress },
   data () {
     return {
       tasks: []
+    }
+  },
+  computed: {
+    progress () {
+      const total = this.tasks.length
+      const done = this.tasks.filter(t => !t.pending).length
+      return Math.round(done / total * 100) || 0
+    }
+  },
+  watch: {
+    tasks: {
+      deep: true,
+      handler () {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      }
     }
   },
   methods: {
@@ -37,10 +54,16 @@ export default {
     deleteTask (i) {
       this.tasks.splice(i, 1)
     },
-    toggleTaskState(i){
+    toggleTaskState (i) {
       this.tasks[i].pending = !this.tasks[i].pending
     }
+  },
+  created () {
+    const json = localStorage.getItem('tasks')
+    const array = JSON.parse(json)
+    this.tasks = Array.isArray(array) ? array : []
   }
+
 }
 </script>
 
