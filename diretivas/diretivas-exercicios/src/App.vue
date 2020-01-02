d2b488ac32a66eae211267e0ef5170c38dd9f716
Rogério de Lima Sousa <rogeriothe@gmail.com>
2020-01-02 07:31:47 -0300

    desafio diretivas

---
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr />
+    <p v-text="'Usando v-text'"></p>
+    <p v-html="'Usando diretiva <strong>strong</strong>'"></p>
+    <p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque-local="cor">Usando diretiva personalizada</p>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "destaque-local": {
+      bind(el, binding, vnode) {
+        el.style.color = 'yellow';
+      }
+    }
+  },
+  data() {
+    return {
+      cor: "red"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
---
+++ diretivas/diretivas-exercicios2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr>
+		<p v-text="'Usando diretiva v-text(recebe conteudo js)'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<p v-destaque:fundo.atrasar="'green'">Usando diretiva destaque</p>
+		<p v-destaque:fundo="'lightblue'">Usando diretiva destaque</p>
+		<p v-destaque.atrasar="cor">Usando diretiva destaque</p>
+
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+    return {
+      cor: "red"
+    };
+  }
+}	
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
