7b31ad8fdec49de01ecc69bb67962f819e5c7a71
Rogério de Lima Sousa <rogeriothe@gmail.com>
2020-01-03 10:00:40 -0300

    filtros e mixins

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <h1>Filtros & Mixins</h1>
+    <hr>
+    <p>{{ cpf | formataCpf | inverter }}</p>
+    <input
+      type="text"
+      :value="cpf | formataCpf | inverter"
+    >
+    <hr>
+    <Frutas />
+    <hr>
+    <div>
+      <ul>
+        <li
+          v-for="fruta in frutas"
+          :key="fruta"
+        >{{ fruta }}</li>
+      </ul>
+      <input
+        type="text"
+        v-model="fruta"
+        @keydown.enter="add"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import frutasMixin from './FrutasMixin'
+import Frutas from './Frutas.vue'
+
+export default {
+  components: { Frutas },
+  mixins: [frutasMixin],
+  filters: {
+    formataCpf (valor) {
+      const arr = valor.split('')
+      arr.splice(3, 0, '.')
+      arr.splice(7, 0, '.')
+      arr.splice(11, 0, '-')
+      return arr.join('')
+    }
+  },
+  data () {
+    return {
+      cpf: '00641402376',
+    }
+  },
+  methods: {
+    
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+
+input {
+  font-size: 2.5rem;
+}
+</style>
---
+++ formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input
+        type="text" @input="alterarNome"
+        :value="value.nome"
+      />
+    </Rotulo>
+
+    <Rotulo nome="Sobrenome">
+      <input
+        type="text" @input="alterarSobrenome"
+        :value="value.sobrenome"
+      />
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from './Rotulo.vue'
+
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  components: { Rotulo },
+  methods: {
+    alterarNome (event) {
+      this.$emit('input', {
+        nome: event.target.value,
+        sobrenome: this.value.sobrenome
+      })
+    },
+    alterarSobrenome (event) {
+      this.$emit('input', {
+        nome: this.value.nome, sobrenome: event.target.value
+      })
+    },
+  }
+}
+</script>
+
+<style>
+.rotulo {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 15px 0px;
+  font-size: 1.4rem;
+}
+
+.elemento {
+  display: flex;
+  align-self: stretch;
+  margin: 10px 0px;
+}
+</style>
